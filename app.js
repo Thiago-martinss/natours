927957d3811fa8bad492ecfbd5fb76eea2a6d1d7
Thiago <martinsmthiago@gmail.com>
2024-09-25 15:45:18 -0300

    add users routes

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+//1) MIDDLEWARES
+app.use(morgan('dev'))
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Logging middleware...');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requesttIME = new Date().toISOString;
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//2) ROUTES  HANDLERS
 const getAllTours = (req, res) => {
     res.status(200).json({
       status: 'success',
@@ -16,7 +31,7 @@ const getAllTours = (req, res) => {
         tours,
       },
     });
-  };
+};
 
   const getTour = (req, res) => {
     console.log(req.params);
@@ -78,8 +93,9 @@ const createTour = (req, res) => {
       }
     }
   );
-}
-  const updateTour = (req, res) => {
+};
+ 
+const updateTour = (req, res) => {
     if (req.params.id * 1 > tours.length) {
       return res.status(404).json({
         status: 'fail',
@@ -93,9 +109,9 @@ const createTour = (req, res) => {
         tour: '<Updated tour here...>',
       },
     });
-  }
+};
 
-  const deleteTour = (req, res) => {
+const deleteTour = (req, res) => {
     if (req.params.id * 1 > tours.length) {
       return res.status(404).json({
         status: 'fail',
@@ -107,18 +123,53 @@ const createTour = (req, res) => {
       status:'success',
       message: 'Tour deleted successfully',
     });
-  }
+};
 
-app.get('/api/v1/tours', getAllTours)
-app.get('/api/v1/tours/:id', getTour)
-app.post('/api/v1/tours', createTour );
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not found',
 });
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not found',
+});
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not found',
+});
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not found',
+});
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Route not found',
+});
+
+//3)ROUTES
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+userRouter.use('/', tourRouter)
+userRouter.use('/:id', userRouter)
+
+//4)START SERVER
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+})
