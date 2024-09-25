a00e32fd851a31bda290e784b2ee7e67c263546f
Thiago <martinsmthiago@gmail.com>
2024-09-25 12:20:48 -0300

    add patch and delete handler

--- app.js
+++ app.js
@@ -1,24 +1,97 @@
-
 const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+
+  const tour = tours.find((t) => t.id === parseInt(id));
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  res.send('success');
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.error(err);
+      status: 'success';
+      data: {
+        tour: newTour;
+      }
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Tour not found',
+      });
+    }
+  
+    res.status(204).json({
+      status: 'success',
+      data: 
+        null
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);   
+  console.log(`Server is running on port ${port}`);
 });
