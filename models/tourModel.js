1adf0680a33c3d033cde7c53074f74b626384f14
Thiago <martinsmthiago@gmail.com>
2024-09-28 21:11:06 -0300

    add tourModel and refactor

---
+++ models/tourModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const tourSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    duration: { type: Number, required: true },
+    difficulty: {
+      type: String,
+      required: true,
+      enum: ['easy', 'medium', 'hard'],
+    },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    ratingsQuantity: { type: Number, default: 0 },
+    //images:  [String],
+    createdAt: { type: Date, default: Date.now },
+    startDate: { type: Date },
+  });
+  
+  const Tour = mongoose.model('Tour', tourSchema);
+
+
+  module.exports = Tour;
