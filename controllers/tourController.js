6f250ae9ab9e893cbab740dde137ccc9e8bd966d
Thiago <martinsmthiago@gmail.com>
2024-09-29 20:12:09 -0300

    add filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,23 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours  = async (req, res) => {
   try {
-    const tours = await Tour.find()
+    //BUILD QUERY
+    //1) Filtering
+    const queryObj = {...req.query};
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach(field => delete queryObj[field]);
+
+    //2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+  
+    
+    const query = Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
     status: 'success',
     results: tours.length,
