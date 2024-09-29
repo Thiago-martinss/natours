aac1e66939efad06c31820da3a2f7fc1baa23865
Thiago <martinsmthiago@gmail.com>
2024-09-28 21:56:44 -0300

    refactor get tour / all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,40 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
+exports.getAllTours  = async (req, res) => {
+  try {
+    const tours = await Tour.find()
+    res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
+    }
   });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+   const tour = await Tour.findById(req.params.id)
+
+   res.status(200).json({
+    status:'success',
+    data: {
+      tour
+    }
+   });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found'
+    });
 };
+}
 
 exports.createTour = async (req, res) => {
   try {
