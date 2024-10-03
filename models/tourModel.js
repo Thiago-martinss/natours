180f3c21f67975b4d06f092c83c88c144084e6d1
Thiago <martinsmthiago@gmail.com>
2024-10-03 16:49:33 -0300

    add middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 
 const tourSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true, trim: true },
+    slug: String,
+    startLocation: { type: String, required: true },
+    locations: [
+      {
     duration: { type: Number, required: true },
     maxGroupSize: { type: Number, required: true},
     difficulty: {
@@ -21,6 +26,7 @@ const tourSchema = new mongoose.Schema({
     guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
     createdAt: { type: Date, default: Date.now, select: false },
     startDates: [Date],
+    secretTour: { type: Boolean, default: false}
   }, 
   {
     toJSON: { virtuals: true },
@@ -32,7 +38,43 @@ const tourSchema = new mongoose.Schema({
     return this.duration / 7;
   })
   
-  const Tour = mongoose.model('Tour', tourSchema);
+
+  // DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('salve', function(next) {
+  console.log('About to save a tour: ', this);
+  next();
+})
+
+tourSchema.post('save', function(doc, next) {
+  console.log('A new tour has been saved: ', doc);
+  next();
+});
+  
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour:  { $ne: true} });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(next) {
+  console.log(`Querry took ${Date.now() - this.start} milliseconds!`)
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } }});
+  next();
+});
+
+// EXPORT THE MODEL
+const Tour = mongoose.model('Tour', tourSchema);
 
 
   module.exports = Tour;
