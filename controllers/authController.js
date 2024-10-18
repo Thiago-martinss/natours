fcb3929ee01d8fec9be629c9c285eac7082612f5
Thiago <martinsmthiago@gmail.com>
2024-10-18 09:08:58 -0300

    add login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+   return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN   // 2 weeks
+    })
+} 
 
 
 // create a new User
@@ -12,9 +19,8 @@ exports.signup = catchAsync (async (req, res, next) => {
             passwordConfirm: req.body.passwordConfirm
         });
         
-        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET , {
-            expiresIn: process.env.JWT_EXPIRES_IN  
-        })
+        const token = signToken(newUser._id);
+       
         
         res.status(201).json({
             status: 'success',
@@ -24,3 +30,28 @@ exports.signup = catchAsync (async (req, res, next) => {
             }
         });
 });
+
+exports.login =  catchAsync(async (req, res, next) => {
+    const {email, password} = req.body;
+    
+    //1) Check if email and password exist
+    if(!email || !password) {
+      return next(new AppError(' Please provide a valid email and password', 400));
+    
+    }
+    //2) Check if user exists and password is correct
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+    
+    
+    // Create and send JWT
+    const token = signToken(user._id);
+    res.status(200).json({
+        status:'success',
+        token
+    })
+});
+     
--- models/userModel.js
+++ models/userModel.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'User must have a password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
@@ -46,13 +47,22 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     // Only run this function if password was actually modified
     if (!this.isModified('password')) return next();
-
-    // Hash the password using bcrypt
+  
+    // Hash the password with cost of 12
     this.password = await bcrypt.hash(this.password, 12);
-    // Delete the passwordConfirm field after hashing it
+  
+    // Delete passwordConfirm field
     this.passwordConfirm = undefined;
     next();
-});
+  });
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
+  ) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+  };
+  
 
 const User = mongoose.model('User', userSchema);
 
