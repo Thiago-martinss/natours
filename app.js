b9eb030aa170583a90fd99fb842d8da49e723016
Thiago <martinsmthiago@gmail.com>
2024-10-23 16:13:21 -0300

    add hpp

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 
 const AppError = require('./utils/appError');
@@ -33,6 +36,19 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({limit: '10kb'}));
+
+// Data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration', 'difficulty', 'price', 'ratingsAverage', 
+    'ratingsQuantity', 'maxGroupSize', 'difficulty'
+     ]
+}));
 
 
 // Serving static files from the public folder
