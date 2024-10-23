a3f2a84babe02298fa352cfb368b6ed7e7100909
Thiago <martinsmthiago@gmail.com>
2024-10-23 15:30:58 -0300

    add rate limit and security http headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 
 const AppError = require('./utils/appError');
@@ -9,14 +11,34 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//GLOBAL MIDDLEWARES
+
+// SECURITY HTTP HEADERS
+app.use(helmet());
+
+
+// Logging middleware for development environment
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again in 15 minutes.'
+});
+
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+
+// Serving static files from the public folder
 app.use(express.static(`${__dirname}/public/`));
 
-
+// Test middleware
 app.use((req, res, next) => {
   req.requesttIME = new Date().toISOString;
   console.log(req.headers)
