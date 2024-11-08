da40e3af2d995cb4a1961ede109e2307e9975e94
Thiago <martinsmthiago@gmail.com>
2024-11-08 16:40:20 -0300

    virtual populate tour, reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -123,6 +123,13 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  localField: '_id',
+  foreignField: 'tour',
+  as: 'reviews'
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
