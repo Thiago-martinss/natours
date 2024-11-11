4cc49957b698be054be746fde5732c5ddc663305
Thiago <martinsmthiago@gmail.com>
2024-11-11 11:38:46 -0300

    refactor update handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,11 +74,8 @@ exports.createUser = (req, res) => {
     message: 'This route is not yet defined!'
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 
-exports.deleteUser = factory.deleteOne(User)
+exports.deleteUser = factory.deleteOne(User);
+
+// DO NOT update passwords with this
+exports.updateUser = factory.updateOne(User);
