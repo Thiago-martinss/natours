a671f9ebc8428d7e507218e440ca7f7cef5d6aea
Thiago <martinsmthiago@gmail.com>
2024-11-11 20:45:47 -0300

    imported data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
+
+
 
 dotenv.config({ path: './config.env' });
 
@@ -20,11 +24,16 @@ mongoose
 
   // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf8'));
+
 
 //IMPORT DATA INTO DB
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, {validateBeforeSave: false});
+        await Review.create(reviews);
         console.log('Data imported successfully...');
         process.exit();
         
@@ -39,6 +48,9 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
+
         console.log('Data deleted successfully...');
         process.exit();
         
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,22 +6,27 @@ const router = express.Router({mergeParams: true});
 
 
 
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect, 
     authController.restrictTo('user'), 
     reviewController.setTourUserIds,
     reviewController.createReview
 );
 
+
+
 router.route('/:id')
-.patch(reviewController.updateReview)
-.delete(reviewController.deleteReview)
 .get(reviewController.getReview)
-
-
+.patch(
+  authController.restrictTo('user', 'admin'),  // only admin or user can update their own review
+  reviewController.updateReview)
+.delete(
+  authController.restrictTo('user', 'admin'),  // only admin or user can delete their own review
+  reviewController.deleteReview
+)
 
   module.exports = router;
