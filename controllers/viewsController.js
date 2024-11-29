433cd7e86602bee39a7926f6a6c5c19dd354b7a9
Thiago <martinsmthiago@gmail.com>
2024-11-29 16:44:14 -0300

    add login page, refactor and bug fixes

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,17 +10,30 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = catchAsync (async (req, res) => {
-    const tour = await Tour.findOne({slug: req.params.slug}).populate({
-      path: 'reviews',
-      populate: {
-        path: 'reviews',
-        fields: 'review rating user'
-      },
-
-    })
-    res.status(200).render('tour', {
-    title:`${tour.name} Tour`,
-    tour
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'reviews rating user',
+  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: `${tour.name}`,
+      tour,
     });
+});
+ 
+exports.getLoginForm = catchAsync(async function(req, res) {
+  res.status(200)
+  .set(
+    'Content-Security-Policy',
+    "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+  )
+  .render('login', {
+    title: 'Login Screen' // or whatever title you wish
+  });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -54,12 +54,15 @@ userSchema.pre('save', async function(next) {
 
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
+
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
+
 userSchema.pre('save', function(next) {
   if (!this.isModified('password') || this.isNew) return next();
+
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
