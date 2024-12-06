af2bb0ef8dddc5102b72dd8df7829b2c7d3f8030
Thiago <martinsmthiago@gmail.com>
2024-12-06 11:18:10 -0300

    multer configuration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,34 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory')
 
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Only images are allowed!', 400), false);
+  }
+};
+
+const upload = multer({ 
+  storage: multerStorage,
+  fileFilter: multerFilter,
+  limits: { fileSize: 1000000 } // 1MB
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
