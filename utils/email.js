9a915480ac03115a9b8ff24862f06704824af7e0
Thiago <martinsmthiago@gmail.com>
2024-12-06 18:39:07 -0300

    add production emails using brevo

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,14 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const { convert } = require('html-to-text');
+const Transport = require("nodemailer-brevo-transport");
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.name = user.firstName;
     this.url = url;
+    this.from = `Thiago Martins <${process.env.EMAIL_FROM}>`;
     //this.subject = 'Your Tour Booking Confirmation';
     /*this.message = `
       Hi ${this.name},
@@ -18,26 +20,32 @@ module.exports = class Email {
     */
   }
  newTransport() {
-    if (!process.env.EMAIL_HOST ||!process.env.EMAIL_PORT ||!process.env.EMAIL_USERNAME ||!process.env.EMAIL_PASSWORD) {
-      console.error('Email environment variables not set.');
-      return;
-    }
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
+      if (process.env.NODE_ENV === 'production') {
+        //SendinBlue now called brevo
+        return nodemailer.createTransport(
+          new Transport({
+            apiKey: `${process.env.SEND_IN_BLUE_API_KEY}`,
+          })
+        );
       }
-    });
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
   }
+
+    
    async send(template, subject) {
     //1) Render HTML based on the template
     const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, 
       { firstName: this.firstName, url: this.url, subject });
 
     const mailOptions = {
-      from: 'Thiago Martins <admin@admin.com>',
+      from: this.from,
       to: this.to,
       subject,
       html,
@@ -50,6 +58,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('Welcome', 'Welcome to the Natours Family!');
   }
+
+  async sendPasswordReset() {
+    await this.send('passwordReset', 'Password Reset');
+  }
+ 
 };
 
 
